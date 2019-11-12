c216b6627de10047c1a03d1501ed85057b02dcca
Gabriela Cuello <gab.jo.cuello@gmail.com>
2019-11-12 14:08:25 -0600

    Add Router, Inventory, and Order components| enable react router dom for routing

---
+++ finni-ecommerce-react/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+// import logo from './logo.svg';
+// import './App.css';
+
+const App = () => (
+    <div className="App">
+      <Header />
+      <Order />
+      <Inventory />
+    </div>
+);
+
+export default App;
---
+++ finni-ecommerce-react/src/components/Order.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Order extends React.Component {
+    render() {
+        return (
+            <div className="order">This!! IS THE ORDER!</div>
+        );
+    }
+}
+export default Order;
